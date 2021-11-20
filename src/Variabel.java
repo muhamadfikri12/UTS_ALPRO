e24fe0df7560d5945765785c0ab36d5b571b310f
Muhamad Fikri <muhamad.fikri@unida.ac.id>
2021-11-20 10:35:25 +0700

    Initial commit

---
+++ src/Variabel.java
@@ -0,0 +1,12 @@
+public class Variabel {
+	public static void main(String args[])
+	{
+		String sw= "Assalamu'alaikum";
+		String hw = "Muhamad Fikri";
+		int x = 2000;
+
+		System.out.println(sw);
+		System.out.println("Nama Saya : " + hw);
+		System.out.println("Saya Lahir di Tahun : " + x);
+	}
+}
